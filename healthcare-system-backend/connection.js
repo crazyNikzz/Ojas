3865ef6a804d06cdbba59b48e44f1124fb4ba1d9
crazyNikzz <118982484+crazyNikzz@users.noreply.github.com>
2024-12-25 12:35:13 +0530

    Delete healthcare-system-backend directory

--- healthcare-system-backend/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/healthcare", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("MongoDB connected!");
-}).catch(err => console.log(err));
